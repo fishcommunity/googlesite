11607b781bd152b364a2506be7daf9bb0d414cd6
fishcommunity <159089447+fishcommunity@users.noreply.github.com>
2024-02-06 23:15:09 -0800

    Update fishing.js

--- fish/fishing.js
+++ fish/fishing.js
@@ -1,17 +1,8 @@
+const urlParams = new URLSearchParams(queryString);
 
 function getCookie(cname) {
-    let name = cname + "=";
-    let ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
+
+    return urlParams.get(cname);
 }
 
 function delay(time) {
